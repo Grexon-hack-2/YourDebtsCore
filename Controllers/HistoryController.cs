26b264bea07287834e9b3ab99402425f7726b18f
Grexon-hack-2 <jsarabialugo2306@gmail.com>
2024-04-16 12:33:16 -0500

    task20240416-1 - ajustes de estadisticas

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YourDebtsCore.Base.Validations;
+using YourDebtsCore.Services;
+
+namespace YourDebtsCore.Controllers
+{
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHistoryService _historyService;
+
+        public HistoryController(IHttpContextAccessor httpContextAccessor, IHistoryService historyService)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _historyService = historyService;
+        }
+
+        [HttpGet]
+        [Route("GetHistoryOtherDebts")]
+        public async Task<IActionResult> GetHistory_otherDebts()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var (currentUser, expired) = ValidationToken.Handler(context);
+            if (expired) return Unauthorized("Token Expirado");
+
+
+            return Ok(await _historyService.GetHistoryOtherDebt(currentUser.UserAdminID));
+        }
+
+        [HttpGet]
+        [Route("GetHistoryAbono")]
+        public async Task<IActionResult> GetHistory_Abono()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var (currentUser, expired) = ValidationToken.Handler(context);
+            if (expired) return Unauthorized("Token Expirado");
+
+
+            return Ok(await _historyService.GetHistoryAbono(currentUser.UserAdminID));
+        }
+
+        [HttpGet]
+        [Route("GetHistoryProducts")]
+        public async Task<IActionResult> GetHistory_Products()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var (currentUser, expired) = ValidationToken.Handler(context);
+            if (expired) return Unauthorized("Token Expirado");
+
+
+            return Ok(await _historyService.GetHistoryProducts(currentUser.UserAdminID));
+        }
+    }
+}
---
+++ Repositories/HistoryRepository.cs
@@ -0,0 +1,93 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+using YourDebtsCore.Base.Models;
+
+namespace YourDebtsCore.Repositories
+{
+    public interface IHistoryRepository
+    {
+        Task<List<History_OtherDebt>> GetHistoryOtherDebts(Guid adminID);
+        Task<List<History_Abono>> GetHistoryAbono(Guid adminID);
+        Task<List<History_Product>> GetHistoryProducts(Guid adminID);
+    }
+    public class HistoryRepository: IHistoryRepository
+    {
+        private string _connectionString;
+        public HistoryRepository(string conn) 
+        { 
+            _connectionString = conn;
+        }
+
+        public async Task<List<History_OtherDebt>> GetHistoryOtherDebts(Guid adminID)
+        {
+            try
+            {
+                using var conn = new SqlConnection(_connectionString);
+                conn.OpenAsync().Wait();
+
+                var text = "SELECT * FROM History_OtherDebts where UserAdminID = @adminID";
+
+                var response = await conn.QueryAsync<History_OtherDebt>(text, new {adminID});
+
+                return response.ToList();
+
+            }
+            catch(SqlException ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<History_Abono>> GetHistoryAbono(Guid adminID)
+        {
+            try
+            {
+                using var conn = new SqlConnection(_connectionString);
+                conn.OpenAsync().Wait();
+
+                var text = "SELECT * FROM History_AbonoDebt where UserAdminID = @adminID";
+
+                var response = await conn.QueryAsync<History_Abono>(text, new { adminID });
+
+                return response.ToList();
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<History_Product>> GetHistoryProducts(Guid adminID)
+        {
+            try
+            {
+                using var conn = new SqlConnection(_connectionString);
+                conn.OpenAsync().Wait();
+
+                var text = "SELECT * FROM History_Products where UserAdminID = @adminID";
+
+                var response = await conn.QueryAsync<History_Product>(text, new { adminID });
+
+                return response.ToList();
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
